9332f5217adc10d190d1a068c4fa930a433c665c
kabir4874 <kabir.cse.bd@gmail.com>
2025-03-28 16:30:58 +0600

    auth

---
+++ controllers/auth.controller.js
@@ -0,0 +1,87 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const register = async (req, res) => {
+  const { name, email, password, phoneNumber } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+      phoneNumber,
+    });
+    await newUser.save();
+    res.status(201).json({
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error on register user" });
+  }
+};
+
+export const Login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    if (!user.emailVerified) {
+      return res.status(400).json({ message: "Please verify your email" });
+    }
+    const token = jwt.sign(
+      { userId: user._id, email: user.email, name: user.name },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      message: "Login successfully",
+      token,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error on login user" });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  const { email, otp, newPassword } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User with this email does not exist" });
+    }
+
+    if (user.emailVerificationCode !== otp) {
+      return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 12);
+
+    user.password = hashedPassword;
+    user.emailVerificationCode = undefined; // Clear the OTP
+
+    await user.save();
+
+    res.status(200).json({ message: "Password has been successfully reset" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
---
+++ controllers/otp.controller.js
@@ -0,0 +1,83 @@
+import nodemailer from "nodemailer";
+import User from "../models/user.model.js";
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  service: process.env.SMTP_SERVICE,
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_MAIL,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP Connection Error:", error);
+  } else {
+    console.log("SMTP Server is ready to take messages");
+  }
+});
+const generateRandomCode = () =>
+  Math.floor(100000 + Math.random() * 900000).toString();
+
+//Send Email OTP
+export const sendEmailOtp = async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: "Email is required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const emailVerificationCode = generateRandomCode();
+    user.emailVerificationCode = emailVerificationCode;
+    await user.save();
+
+    const mailOptions = {
+      from: process.env.SMTP_MAIL,
+      to: user.email,
+      subject: "Verification Code",
+      text: `Your verification code is: ${emailVerificationCode}. It is valid for 5 minutes.`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res
+      .status(200)
+      .json({ message: "Email verification code sent to your email" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
+};
+
+// Verify Email OTP
+export const verifyEmailOtp = async (req, res) => {
+  const { email, otp } = req.body;
+
+  if (!email || !otp) {
+    return res.status(400).json({ message: "Email and OTP are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user || String(user.emailVerificationCode) !== String(otp)) {
+      return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    user.emailVerified = true;
+    user.emailVerificationCode = null;
+    await user.save();
+
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
+};
