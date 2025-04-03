d43f5b2bdce4a6b5bc8779590e7d27243787cbe3
kabir4874 <kabir.cse.bd@gmail.com>
2025-04-03 14:31:53 +0600

    admin login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import Admin from "../models/admin.model.js";
 import User from "../models/user.model.js";
-
 export const register = async (req, res) => {
   const { name, email, password, phoneNumber } = req.body;
   try {
@@ -83,5 +83,38 @@ export const resetPassword = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const adminLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const admin = await Admin.findOne({ email });
+    if (!admin) {
+      return res.status(400).json({ message: "Admin not found" });
+    }
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      {
+        adminId: admin._id,
+        email: admin.email,
+        name: admin.name,
+        role: "admin",
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      message: "Login successfully",
+      token,
+      admin,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error on login admin" });
   }
 };
