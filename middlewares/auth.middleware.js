51436d3c1ae5e4df627c34859a2ffe075d356471
kabir4874 <kabir.cse.bd@gmail.com>
2025-04-03 14:27:49 +0600

    admin middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
+import Admin from "../models/admin.model.js";
+import User from "../models/user.model.js";
 
-export const authMiddleware = (req, res, next) => {
+export const authMiddleware = async (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
@@ -13,6 +15,43 @@ export const authMiddleware = (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = decoded;
+
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      return res
+        .status(403)
+        .json({ message: "Access denied. User is not find." });
+    }
+
+    next();
+  } catch (error) {
+    console.error("JWT Error:", error);
+    res.status(401).json({ message: "Invalid or expired token." });
+  }
+};
+
+export const adminMiddleware = async (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+
+    const adminUser = await Admin.findById(req.user._id);
+
+    if (!adminUser) {
+      return res
+        .status(403)
+        .json({ message: "Access denied. User is not an admin." });
+    }
 
     next();
   } catch (error) {
